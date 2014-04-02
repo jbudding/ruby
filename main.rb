a294ac106992d48e1b6f19775b6729adf6dccd1e
abuddington <austin.j.buddington@gmail.com>
2014-04-02 04:03:45 -0400

    set up welcome message and started user input handling

---
+++ main.rb
@@ -0,0 +1,29 @@
+class StoryTeller
+    def initialize(title)
+        @title = title
+
+        welcome = <<-MESSAGE
+        ---------------------------------------
+        would you like to read about #{@title}?
+        ---------------------------------------
+        [Y] / [N]
+        MESSAGE
+
+        puts welcome
+        proceed?
+    end
+
+    def proceed?
+        inp = gets
+
+        if inp == "Y"
+            puts "YAY!!!"
+        elsif inp == "N"
+            puts "NOOOO!!!"
+        else
+            puts "please enter a valid option:\t[Y] / [N]"
+        end
+    end
+end
+
+s = StoryTeller.new("Bailey's run")
